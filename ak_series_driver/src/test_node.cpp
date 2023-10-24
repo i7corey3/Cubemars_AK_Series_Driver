288b387d863acfd3d8ae6826c37e847f07e9cf08
corey <coreyleydig@gmail.com>
2023-10-24 17:01:15 -0400

    update

--- ak_series_driver/src/test_node.cpp
+++ ak_series_driver/src/test_node.cpp
@@ -31,71 +31,14 @@ class TestNode : public rclcpp::Node
 
 };
 
-void mcp2515_select(void)
-{}
 
-void mcp2515_deselect()
-{}
 
-uint8_t mcp2515_spi_transfer(uint8_t data)
-{
-    return data;
-}
-
-void delay(uint32_t ms)
-{
-  for (; ms > 0; ms--)
-  {
-    for (uint16_t j = 0; j < 25000; j++)
-    {
-      __asm__("nop"); /* Do nothing. */
-    }
-  }
-}
-
-void sendCanData(uint32_t id, uint8_t dlc, uint8_t *data)  
-{
-    can_frame frame;
-    frame.can_id = id;
-    frame.can_dlc = dlc;
-    for (int i = 0; i < dlc; i++)
-    {
-        frame.data[i] = data[i];
-    }
-    
-    mcp2515->sendMessage(&frame);
-}
 
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
     
-    mcp2515 = new MCP2515(&mcp2515_select,
-                &mcp2515_deselect,
-                &mcp2515_spi_transfer,
-                &delay);
-
-    uint8_t motorId = 1;
-    mcp2515->reset();
-    mcp2515->setBitrate(CAN_1000KBPS, MCP_8MHZ);
-    mcp2515->setNormalMode();
-
-    TMotorActuators::AkActuators ak10 = TMotorActuators::AkActuators(motorId,
-                                                            TMotorActuators::ak60_6_v1_1,
-                                                            sendCanData);
-
-    delay(1000);
-    ak10.enable();
-    delay(1000);
-
-    ak10.move(0.0, -50, 0, 0.4, 0.2);
-    delay(1000);
-
-    ak10.move(3.1415, -50, -10, 2, 0.2);
-    delay(1000);
-
-    ak10.disable();
-    delay(1000);
+  
                 
    
     // rclcpp::spin(std::make_shared<TestNode>());
